9bce464f6cb248264fe63c820205fc1f70080f31
yumin22 <22joomin@naver.com>
2024-10-23 21:34:18 +0900

    2444번 간략화

--- python/2444.py
+++ python/2444.py
@@ -1,12 +1,12 @@
 n = int(input())
 
 for i in range(1, n+1):
-    print(" "*(n-i)+"*"*i, end="")
-    print("*"*(i-1))
+    print(" "*(n-i)+"*"*(2*i-1))
+
 
 for j in range(1, n):
-    print(" "*j+"*"*(n-j), end="")
-    print("*"*(n-1-j))
+    print(" "*j+"*"*(2*(n-j)-1))
+
 
     
     
