9257a2bb3ce11b724aaaca9cd7919d41a4567562
yumin22 <22joomin@naver.com>
2024-11-13 12:11:24 +0900

    11/13 완료

---
+++ 9506.py
@@ -0,0 +1,16 @@
+while True:
+    n = int(input())
+
+    if n == -1:
+        break
+
+    divisor = []
+    for i in range(1, n):
+        if (n % i == 0):
+            divisor.append(i)
+
+    if (n== sum(divisor)):
+        print(n, " = ", " + ".join(str(i) for i in divisor), sep="")
+    else:
+        print(n,  "is NOT perfect.")
+
